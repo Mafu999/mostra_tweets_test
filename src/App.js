6a301fd592b6f4de3a262f1de8674061260b5d4a
Pedro <mafuzao@outlook.com>
2021-09-28 16:36:54 -0300

    Consumindo API e filtrando a # através de uma variável

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+//Importando dependencias no app
+const express = require('express')
+const Twit = require('twit')
+//instanciando express para uso no app
+const app = express()
+//Importando dados de tolken
+const T = require('./Token/token')
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
 
-export default App;
+
+
+
+
+let contentOfTweet = '%23GenshinImpact'
+app.get('/tweets', (request, response)=>{
+
+    T.get(`https://api.twitter.com/1.1/search/tweets.json?q=${contentOfTweet}&result_type=recent`, (err, data, response)=>{
+        
+            console.log(data)
+        
+    })
+
+    response.sendStatus(200)
+})
+
+app.listen(1112)
