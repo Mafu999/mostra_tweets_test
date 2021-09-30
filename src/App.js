24c30ba73b7129eef081f3f026c7a77ccf4ad0f4
Pedro <mafuzao@outlook.com>
2021-09-29 22:15:09 -0300

    tratando dados das requisições, criando uma model e apagando alguns arquivos inúteis

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //Importando dependencias no app
+const { query } = require('express')
 const express = require('express')
 const Twit = require('twit')
 //instanciando express para uso no app
@@ -6,21 +7,40 @@ const app = express()
 //Importando dados de tolken
 const T = require('./Token/token')
 
+//Essa variável é onde fica guardado o hashtag desejado. o codigo %23 é uma hashtag em url encode.
+let contentOfTweet = '%23GTA'
 
-
-
-
-
-let contentOfTweet = '%23GenshinImpact'
 app.get('/tweets', (request, response)=>{
 
     T.get(`https://api.twitter.com/1.1/search/tweets.json?q=${contentOfTweet}&result_type=recent`, (err, data, response)=>{
+    //const result = [data.statuses.id, data.statuses.user, data.statuses.text]    
+    try{
+            //var percorreDados = ()=>{ for(let i = 0; i < data.statuses.length; i++){  
+               let parametros = data.statuses.map(function(elemen){
+                return [{
+                    id: elemen.user.id, 
+                    nome: elemen.user.name,
+                    nome_de_usuario: elemen.user.screen_name,
+                    texto: elemen.text,
+                    hashtags: elemen.entities.hashtags.slice(0,30)
+                }]
+              })
+              console.log(parametros)
+              
+            }
+            
         
-            console.log(data)
-        
+        //console.log(percorreDados())
+     
+        catch(err){
+            //é necessário esse if?
+            if (err){
+            console.log(`${err}`)
+            }
+        }  
     })
-
     response.sendStatus(200)
 })
-
 app.listen(1112)
+
+module.exports = parametros
---
+++ src/Model/tweet.js
@@ -0,0 +1,10 @@
+class Data {
+    constructor({
+        id, created_at, text, user
+    }){
+        this.id = id
+        this.created_at= created_at
+        this.text= text
+        this.user = user
+    }
+}
